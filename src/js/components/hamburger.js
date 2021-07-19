da39eac54d5d60b5db2f345e382529cb11c2af12
nBlackNinja <nikitayudin782@gmail.com>
2021-07-19 21:50:12 +0300

    добавил конфиденциальность

--- src/js/components/hamburger.js
+++ src/js/components/hamburger.js
@@ -5,3 +5,19 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("is-active");
   mobileMenu.classList.toggle("mobile__menu-is-active");
 });
+
+const mobileSubmit = document.querySelector(".mobile__menu-button");
+
+mobileSubmit.addEventListener("click", function () {
+  hamburger.classList.toggle("is-active");
+  mobileMenu.classList.toggle("mobile__menu-is-active");
+});
+
+const list = document.querySelectorAll(".mobile__menu-link");
+
+list.forEach((i) => {
+  i.addEventListener("click", function (e) {
+    hamburger.classList.toggle("is-active");
+    mobileMenu.classList.toggle("mobile__menu-is-active");
+  });
+});
