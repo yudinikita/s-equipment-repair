3dc0e734e14fc8af71c0076198084ca58f6f0223
nBlackNinja <nikitayudin782@gmail.com>
2021-07-20 01:03:10 +0300

    добавил срабатывание кнопки в подвале на форму

--- src/js/components/showform.js
+++ src/js/components/showform.js
@@ -1,10 +1,15 @@
 const phone = document.querySelector(".header__phone");
 const form = document.querySelector(".formcallback");
+const btnCallback = document.querySelector(".footer__backcall-link");
 
-phone.addEventListener("mouseover", function () {
+phone.addEventListener("mouseenter", function () {
   form.classList.toggle("showform");
 });
 
-form.addEventListener("mouseout", function () {
+form.addEventListener("mouseleave", function () {
+  form.classList.toggle("showform");
+});
+
+btnCallback.addEventListener("click", function () {
   form.classList.toggle("showform");
 });
