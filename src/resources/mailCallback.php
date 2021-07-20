6e488a2b645e290f3b0175e61c786cc7a3222c0d
nBlackNinja <nikitayudin782@gmail.com>
2021-07-20 15:58:18 +0300

    1

--- src/resources/mailCallback.php
+++ src/resources/mailCallback.php
@@ -12,11 +12,14 @@ $tel = urlencode($tel);
 $name = trim($name);
 $tel = trim($tel);
 
+$headers= "MIME-Version: 1.0\r\n";
+$headers .= "Content-type: text/html; charset=utf-8";
+
 if (mail("nikitayudin782@gmail.com",
-     "Pest Reject",
+     "Частные мастера",
      "Имя: ".$name."\n".
      "Телефон ".$tel,
-     "From: mail@bytovoi-remont.ru \r\n")
+     "From: mail@bytovoi-remont.ru \r\n", $headers)
 ){
      echo "Ваша заявка отправлена! Наш мастер перезвонит в течение 15 минут. Ожидайте звонка.";
 }
