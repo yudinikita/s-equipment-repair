b0c8275a2de22f840abac1a9ba31558a4ec70db2
nBlackNinja <nikitayudin782@gmail.com>
2021-07-27 14:12:23 +0300

    добавил выделение для сообщения на почте

--- src/resources/mailCallback.php
+++ src/resources/mailCallback.php
@@ -12,7 +12,7 @@ $tel = trim($tel);
 
 $to = "nikitayudin782@gmail.com";
 $title = "Обратный звонок • Частные мастера";
-$text = "Имя: ".$name."<br/>"."Телефон: ".$tel;
+$text = "<b>Имя:</b> ".$name."<br/>"."<b>Телефон:</b> ".$tel;
 $headers = "MIME-Version: 1.0\r\n";
 $headers .= "Content-type: text/html; charset=UTF-8";
 $headers .= "From: Частные мастера <mail@bytovoi-remont.ru>\r\n";
